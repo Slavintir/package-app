1c24a31c2c6058a26e9129d1bead08f313b56123
vyacheslav <govras99@outlook.com>
2020-04-12 02:03:17 +0300

    edited index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import { AppOptions } from './src/interfaces/app';
+
 export * from './src/decorators/actions';
 export * from './src/errors/action'
 export * from './src/interfaces/app';
@@ -5,5 +7,7 @@ export * from './src/interfaces/app';
 declare module 'package-app' {
     export class App {
         start(): Promise<void>;
+
+        static getInstance(options?: AppOptions): App;
     }
 }
