3fdd8ecaf6f8feb1278086eec26dfe6af2e51daa
vyacheslav <govras99@outlook.com>
2020-08-16 16:38:03 +0300

    updated types

--- dist/interfaces/app/index.js
+++ dist/interfaces/app/index.js
@@ -1,3 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var actions_1 = require("./actions");
+Object.defineProperty(exports, "ActionName", { enumerable: true, get: function () { return actions_1.ActionName; } });
 //# sourceMappingURL=index.js.map
--- dist/transports/moleculer.js
+++ dist/transports/moleculer.js
@@ -27,7 +27,7 @@ class MoleculerTransport {
                 const actionDir = _c.value;
                 if (expansions.includes(path_1.extname(actionDir))) {
                     const { actionName, handler } = require(actionDir).default;
-                    actions[actionName] = handler;
+                    actions[actionName] = async (ctx) => { await handler(ctx.params); };
                 }
             }
         }
